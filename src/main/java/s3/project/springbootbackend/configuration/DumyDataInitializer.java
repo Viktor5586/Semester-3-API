1b84943114655039402081ed2864b0451e098b85
Doychev,Viktor V <v.doychev@student.fontys.nl>
2022-10-07 16:28:40 +0300

    Fixed creating fake data

--- src/main/java/s3/project/springbootbackend/configuration/DumyDataInitializer.java
+++ src/main/java/s3/project/springbootbackend/configuration/DumyDataInitializer.java
@@ -15,10 +15,10 @@ public class DumyDataInitializer {
     @EventListener(ApplicationReadyEvent.class)
     public void initialDummyData(){
         if (userRepository.findAll().size() == 0){
-            userRepository.save(UserEntity.builder().id(1L).username("Gepi").password("asdfghjk").build());
-            userRepository.save(UserEntity.builder().id(2L).username("Gogi").password("asdfghjk").build());
-            userRepository.save(UserEntity.builder().id(3L).username("Ko").password("asdfghjk").build());
-            userRepository.save(UserEntity.builder().id(4L).username("Ne").password("asdfghjk").build());
+            userRepository.save(UserEntity.builder().username("Gepi").password("asdfghjk").build());
+            userRepository.save(UserEntity.builder().username("Gogi").password("asdfghjk").build());
+            userRepository.save(UserEntity.builder().username("Ko").password("asdfghjk").build());
+            userRepository.save(UserEntity.builder().username("Ne").password("asdfghjk").build());
         }
     }
 
--- src/main/java/s3/project/springbootbackend/controller/UserController.java
+++ src/main/java/s3/project/springbootbackend/controller/UserController.java
@@ -19,6 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class UserController {
     private GetUserUseCase userUseCase;
     private GetAllUsersUseCase getAllUsersUseCase;
