6eff41ff3e33241eb3aedea2e9790b67dcf29a8c
Doychev,Viktor V <v.doychev@student.fontys.nl>
2022-11-04 18:25:04 +0200

    Refactoring

--- src/main/java/s3/project/springbootbackend/persistence/repositories/TruckRepository.java
+++ src/main/java/s3/project/springbootbackend/persistence/repositories/TruckRepository.java
@@ -1,7 +1,6 @@
 package s3.project.springbootbackend.persistence.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import s3.project.springbootbackend.persistence.Entities_DTO.TruckEntity;
 
 import java.util.List;
